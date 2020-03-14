bec4f0e763ff981a9753b68af39337a067ada490
JackBen2019 <jackkbennett17@gmail.com>
2020-03-14 10:05:06 +0000

    Routing for all pages added within run.py

---
+++ .theia/run.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/recipes")
+def recipes():
+    return render_template("/recipes.html")
+
+@app.route("/shop")
+def shop():
+    return render_template("/shop.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("/blog.html")
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
