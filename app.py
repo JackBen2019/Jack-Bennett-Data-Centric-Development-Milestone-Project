8172be5e0d9e65287e12cfef9e9f829f2017620a
JackBen2019 <jackkbennett17@gmail.com>
2020-04-08 16:35:43 +0000

    Updated requirements.txt file due to issues with Heroku

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from os import path
-if path.exists("env.py"):
-  import env
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
-app.secret_key = os.getenv("SECRET", "randomstring123")
-messages = []
 
 app.config["MONGO_DBNAME"] = 'uploadedRecipes'
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
